95329a7fabb87a900f6265a514be82e616fe3fd8
maximilliandeval <71948163+maximilliandeval@users.noreply.github.com>
2021-01-08 04:32:39 -0500

    Clarified implementation

--- DisjointSets.java
+++ DisjointSets.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 /*
 Disjoint-Set data structure
-Uses the weighted quick-union with path compression implementation
+Implemented with weighted-union-by-rank and path compression
 */
 
 public class DisjointSets<T> {
-
+    // Map items to parents
     private HashMap<T, T> parents = new HashMap<T, T>();
+    // Map items to rank
     private HashMap<T, Integer> ranks = new HashMap<T, Integer>();
+    // Tracks number of sets
     private int count;
 
     public DisjointSets() {
@@ -23,26 +25,25 @@ public class DisjointSets<T> {
         return count;
     }
 
-    // Add a new element in a disjoint singleton set
+    // Add a new item in a disjoint singleton set
     public void insert(T item) {
         parents.put(item, item);
         ranks.put(item, 0);
         count++;
     }
 
-    // Identify the set to which an element belongs
+    // Identify the set to which an element belongs using recursion
     public T find(T item) {
         T parent = parents.get(item);
         if (parent == null) return null;
         if (parent.equals(item)) return item;
-
+        // Reattach all visited elements to the root item (path compression)
         parent = find(parent);
         parents.put(item, parent);
         return parent;
     }
 
-    // Given two elements, union the sets of which they are a part
-    // (uses weighted-union-by-rank)
+    // Given two elements, union the sets of which they belong to
     public boolean union(T item1, T item2) {
         T parent1 = find(item1);
         T parent2 = find(item2);
@@ -55,16 +56,17 @@ public class DisjointSets<T> {
             insert(item2);
             parent2 = item2;
         }
-
+        // Return false if they are already part of the same set
         if (parent1.equals(parent2)) return false;
 
         int rank1 = ranks.get(parent1);
         int rank2 = ranks.get(parent2);
+        // Always attach the shorter tree to the root of the taller tree
         if (rank1 < rank2) {
             parents.put(parent1, parent2);
-        }
-        else {
+        } else {
             parents.put(parent2, parent1);
+            // Update rank
             if (rank1 == rank2)
                 ranks.put(parent1, rank1+1);
         }
